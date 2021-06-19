6c1d23ba6a2322bd7d0a28e85de00cb2b8779de2
Violetaaa <Valvarezareces@gmail.com>
2021-06-19 19:53:36 +0200

    set dark mode as default

--- src/app/services/toggle-theme.service.ts
+++ src/app/services/toggle-theme.service.ts
@@ -15,47 +15,67 @@ export class toggleThemeService {
   private readonly LIGHT_THEME_VALUE = 'LIGHT';
   private readonly DARK_THEME_CLASS_NAME = 'theme-dark';
 
-  private darkThemeSelected = false;
-
+  // private darkThemeSelected = false;
+  private lightThemeSelected = false;
   constructor(private storage: StorageService) {
 
   }
 
+  // public setThemeOnStart() {
+  //   if (this.isDarkThemeSelected()) {
+  //     this.setDarkTheme();
+  //   } else {
+  //     this.setLightTheme();
+  //   }
+  //   setTimeout(() => {
+  //     document.body.classList.add('animate-colors-transition');
+  //   }, 500);
+  // }
+
+
   public setThemeOnStart() {
-    if (this.isDarkThemeSelected()) {
-      this.setDarkTheme();
-    } else {
+    if (this.isLightThemeSelected()) {
       this.setLightTheme();
+    } else {
+      this.setDarkTheme();
     }
     setTimeout(() => {
       document.body.classList.add('animate-colors-transition');
     }, 500);
   }
 
+
+
   public toggle() {
-    if (this.darkThemeSelected) {
-      this.setLightTheme();
-    } else {
+    if (this.lightThemeSelected) {
       this.setDarkTheme();
+    } else {
+      this.setLightTheme();
     }
   }
 
-  private isDarkThemeSelected(): boolean {
-    this.darkThemeSelected = this.storage.get(this.THEME_KEY) === this.DARK_THEME_VALUE;
-    return this.darkThemeSelected;
+  // private isDarkThemeSelected(): boolean {
+  //   this.darkThemeSelected = this.storage.get(this.THEME_KEY) === this.DARK_THEME_VALUE;
+  //   return this.darkThemeSelected;
+  // }
+
+  private isLightThemeSelected(): boolean {
+    this.lightThemeSelected = this.storage.get(this.THEME_KEY) === this.LIGHT_THEME_VALUE;
+    return this.lightThemeSelected;
   }
+
 
   private setLightTheme() {
     this.storage.set(this.THEME_KEY, this.LIGHT_THEME_VALUE);
     document.body.classList.remove(this.DARK_THEME_CLASS_NAME);
-    this.darkThemeSelected = false;
+    this.lightThemeSelected = true;
     this.theme$.next(ThemeMode.LIGHT);
   }
 
   private setDarkTheme() {
     this.storage.set(this.THEME_KEY, this.DARK_THEME_VALUE);
     document.body.classList.add(this.DARK_THEME_CLASS_NAME);
-    this.darkThemeSelected = true;
+    this.lightThemeSelected = false;
     this.theme$.next(ThemeMode.DARK);
   }
 }
